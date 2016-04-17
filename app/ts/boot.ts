ca314ac319aadea385b01234bc01bd801af78e86
dxaraujo <danielxaraujo@gmail.com>
2016-04-17 20:20:23 -0300

    Evolução

--- app/ts/boot.ts
+++ app/ts/boot.ts
@@ -1,8 +1,8 @@
 import {provide} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent,[
-    ROUTER_PROVIDERS
+    ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
