8ac61ca4442842304af2e4b12cc3fcd6715638fa
dxaraujo <danielxaraujo@gmail.com>
2016-04-18 02:20:21 -0300

    Evolução

--- app/ts/component/panel.component.ts
+++ app/ts/component/panel.component.ts
@@ -1,32 +1,31 @@
-declare var $:JQueryStatic;
+declare var $: JQueryStatic;
 
 export abstract class PanelComponent {
 
     public collapsePanel(event) {
         var ibox: JQuery = $(event.target).closest('div.ibox');
         var button: JQuery = $(event.target).closest('i');
-        var content: JQuery = ibox.find('div.ibox-content');
+        var content: JQuery = ibox.closest('div.ibox-content');
         content.slideToggle(200);
         button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
         ibox.toggleClass('').toggleClass('border-bottom');
-        setTimeout(function () {
+        setTimeout(function() {
             ibox.resize();
-            ibox.find('[id^=map-]').resize();
         }, 50);
     }
 
-    public closePanel (event) {
-        var content = $(event.target).closest('div.ibox');
+    public closePanel(event) {
+        var content: JQuery = $(event.target).closest('div.ibox');
         content.remove();
     }
 
     public fullScreenPanel(event) {
-        var ibox = $(event.target).closest('div.ibox');
-        var button = $(event.target).closest('i');
+        var ibox: JQuery = $(event.target).closest('div.ibox');
+        var button: JQuery = $(event.target).closest('i');
         jQuery('body').toggleClass('fullscreen-ibox-mode');
         button.toggleClass('fa-expand').toggleClass('fa-compress');
         ibox.toggleClass('fullscreen');
-        setTimeout(function () {
+        setTimeout(function() {
             $(window).trigger('resize');
         }, 100);
     }
